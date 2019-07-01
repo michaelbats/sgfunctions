9a43462e5d0f861d4f2e64b64916ad6a67a69516
Sebastian Gug <sebastian.gug@ishango.co.uk>
2019-07-01 23:38:59 +0100

    trying stuff

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,29 +4,30 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 export const addAuthor = functions
-  .region('europe-west1')
-  .https.onCall((data, context) => {
-    if (context.auth) {
-      if (context.auth.token.owner !== true) {
-        return { error: 'Request not authorized. User must be owner.' };
-      } else {
-        const email = data.email;
-        return grantAuthorRole(email).then(() => {
-          return { result: 'Added author claims to ' + email };
-        });
-      }
-    } else {
-      return { error: 401 };
-    }
-  });
+    .region('europe-west1')
+    .https.onCall((data, context) => {
+        console.log(context.auth);
+        if (context.auth) {
+            if (context.auth.token.owner !== true) {
+                return { error: 'Request not authorized. User must be owner.' };
+            } else {
+                const email = data.email;
+                return grantAuthorRole(email).then(() => {
+                    return { result: 'Added author claims to ' + email };
+                });
+            }
+        } else {
+            return { error: 401 };
+        }
+    });
 
 async function grantAuthorRole(email: string): Promise<void> {
-  const user = await admin.auth().getUserByEmail(email);
-  if (user.customClaims && (user.customClaims as any).author === true) {
-    return;
-  } else {
-    return admin.auth().setCustomUserClaims(user.uid, {
-      author: true
-    });
-  }
+    const user = await admin.auth().getUserByEmail(email);
+    if (user.customClaims && (user.customClaims as any).author === true) {
+        return;
+    } else {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            author: true
+        });
+    }
 }
